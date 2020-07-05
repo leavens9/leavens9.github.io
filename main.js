1613e3e72da1ca27d90c76ca42770d26b467e5e2
Katie Leavens <kd_one@hotmail.com>
2020-07-05 14:00:54 -0700

    fixed z_index spaces

--- main.js
+++ main.js
@@ -101,7 +101,7 @@ var layer_list = [
 	{
 		'image': quick,
 		'src': './images/layer_9_1.png',
-		'z_index': 1.25,
+		'z_index': 1,
 		'position': {x: 0, y: 0},
 		'blend': null,
 		'opacity': 1
@@ -109,7 +109,7 @@ var layer_list = [
 	{
 		'image': fox,
 		'src': './images/layer_10_1.png',
-		'z_index': 1.75,
+		'z_index': 1.5,
 		'position': {x: 0, y: 0},
 		'blend': null,
 		'opacity': 1
@@ -117,7 +117,7 @@ var layer_list = [
 	{
 		'image': jump,
 		'src': './images/layer_11_1.png',
-		'z_index': 2,
+		'z_index': 1.75,
 		'position': {x: 0, y: 0},
 		'blend': null,
 		'opacity': 1
@@ -125,7 +125,7 @@ var layer_list = [
 	{
 		'image': grass,
 		'src': './images/layer_12_1.png',
-		'z_index': 2.25,
+		'z_index': 2.5,
 		'position': {x: 0, y: 0},
 		'blend': null,
 		'opacity': 1
